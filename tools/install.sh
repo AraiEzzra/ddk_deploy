2bd5da7713655da11342812cca3a7920f996c0ac
Dmytro Mekhed <enshteynn@ukr.net>
2018-10-02 17:45:15 +0300

    public_node: change ssh to https

--- tools/install.sh
+++ tools/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-git clone git@github.com:AraiEzzra/DDKOIN.git
-git clone -b sk-master git@github.com:AraiEzzra/DDKUI.git
+git clone https://github.com/AraiEzzra/DDKOIN.git
+git clone -b sk-master https://github.com/AraiEzzra/DDKUI.git
 cp config/* DDKUI/
 cp config/* DDKOIN/
 cp docker/* .
