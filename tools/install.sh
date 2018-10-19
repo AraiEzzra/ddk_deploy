6e1b32f703f1cd23a86c2de9f84e9778900e451c
Dmytro Mekhed <enshteynn@ukr.net>
2018-10-19 13:30:11 +0300

    local_docker: add stable version

--- tools/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-git clone https://github.com/AraiEzzra/DDKOIN.git
-git clone -b sk-master https://github.com/AraiEzzra/DDKUI.git
-cp config/* DDKUI/
-cp config/* DDKOIN/
-cp docker/* .
