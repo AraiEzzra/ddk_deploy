d6b994d6e16abcc711476ef0581d9936a45a67d0
yarphen <sheremet.mykhaylo@gmail.com>
2018-10-30 15:44:36 +0200

    Use env variables for ports

--- tools/docker/docker-entrypoint.sh
+++ tools/docker/docker-entrypoint.sh
@@ -3,9 +3,9 @@
 echo $(node -v)
 # needs defined db, elasticsearch and redis in docker-compose
 # and wait-port npm package installed
-wait-port $DB_HOST:5432 && \
+wait-port $DB_HOST:$DB_PORT && \
 wait-port $ELASTICSEARCH_HOST && \
-wait-port $REDIS_HOST:6379 && \
+wait-port $REDIS_HOST:$REDIS_PORT && \
 if [[ -v DEBUG ]]; then
   npm run debug
 else
