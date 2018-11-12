386b4206d3ae061fa4c7926a647bb1f2d5ae0f4d
yarphen <sheremet.mykhaylo@gmail.com>
2018-11-12 16:49:54 +0200

    Docker-compose for tests

---
+++ clean.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+cd 10.6.0.1
+docker-compose down --volumes
+cd ../10.7.0.1
+docker-compose down --volumes
+cd ../10.5.0.1
+docker-compose down --volumes
--- test_core.sh
+++ test_core.sh
@@ -1,2 +1,10 @@
-. ./build_core_tag.sh $1 && \
-docker run --entrypoint '' "ddkoin/core:latest" npm test
+# pass git tag or revision you want to build as first argument
+CURRENT_TAG=`cd data/DDKOIN && git tag --points-at HEAD | head -1` && \
+TAG_NAME=${1:-CURRENT_TAG} && \
+REVISION_ID=$1 && \
+cd data/DDKOIN && \
+[ -z "$1" ] || { git fetch --all --tags --prune && git checkout "${REVISION_ID}"; } && \
+cd ../../test && \
+docker-compose up -d --build db redis elasticsearch && \
+docker-compose up --build ddk && \
+docker-compose down --volumes
