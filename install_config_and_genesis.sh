080f8e2b8862d82da375aa92670077d3131c863b
Dmytro Mekhed <enshteynn@ukr.net>
2018-11-13 17:47:48 +0200

    master: rename DDKOIN to DDKCORE

--- install_config_and_genesis.sh
+++ install_config_and_genesis.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 cd data
 cp ../tools/config/* DDKUI/
-cp ../tools/config/* DDKOIN/
-cp -f ../tools/genesisBlock.json DDKOIN/
+cp ../tools/config/* DDKCORE/
+cp -f ../tools/genesisBlock.json DDKCORE/
 cp ../tools/docker/docker-entrypoint.sh .
 cp ../tools/docker/Dockerfile.CORE .
 cp ../tools/docker/Dockerfile.PGDB .
