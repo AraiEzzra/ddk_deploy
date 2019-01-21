77726353d688cb3e5c0db45fb30fbdece899bee3
Werdffelynir <werdffelynir@gmail.com>
2019-01-21 17:36:19 +0200

    integrate unit test

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker network create --subnet=10.0.0.0/8 --gateway=10.0.0.1 docker_ddk-testnet
+docker network create --subnet=10.0.0.0/9 --gateway=10.0.0.1 docker_ddk-testnet
 cd 10.6.0.1
 docker-compose build ddk
 #docker-compose build ddkui uncomment if work with UI
@@ -10,4 +10,4 @@ cd 10.7.0.1
 docker-compose up -d --build
 cd ../
 cd 10.5.0.1
-docker-compose up --build
+docker-compose up --build && docker-compose stop ddktest
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+cd ./10.6.0.1
+docker-compose up --build ddktest
+cd ../
