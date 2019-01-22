572281c89faea22e3335abf7d54b07d3638b37e0
Werdffelynir <werdffelynir@gmail.com>
2019-01-22 12:16:25 +0200

    fixed startup of containers without ddktest

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ docker network create --subnet=10.0.0.0/9 --gateway=10.0.0.1 docker_ddk-testnet
 cd 10.6.0.1
 docker-compose build ddk
 #docker-compose build ddkui uncomment if work with UI
-docker-compose up -d
+docker-compose up -d watch ddk ddkui db redis elasticsearch nginx
 cd ../
 cd 10.7.0.1
 docker-compose up -d --build
