699c36ec229d0539bebfec2ec6123d0b097d100a
Aleksandr Shevchenko <axelsheva94@gmail.com>
2019-02-20 17:02:56 +0200

    Change docker-compose files to new core

--- start.sh
+++ start.sh
@@ -3,8 +3,7 @@
 docker network create --subnet=10.0.0.0/9 --gateway=10.0.0.1 docker_ddk-testnet
 cd 10.6.0.1
 docker-compose build ddk
-#docker-compose build ddkui uncomment if work with UI
-docker-compose up -d watch ddk ddkui db redis elasticsearch nginx
+docker-compose up -d
 cd ../
 cd 10.7.0.1
 docker-compose up -d --build
