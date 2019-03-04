5793ed423b99ad75a577e2aa5adf447210490f13
Dmytro Mehed <d.mehed@sk-consulting.com.ua>
2019-03-04 17:00:30 +0200

    new-core: add db and change install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 mkdir data
 cd data
-git clone -b sk-master https://github.com/AraiEzzra/DDKCORE.git
+git clone -b split-api-core https://github.com/AraiEzzra/DDKCORE.git
 git clone -b sk-master https://github.com/AraiEzzra/DDKUI.git
 cd ..
 sh volume-clean.sh
