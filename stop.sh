ad626abd8cef01d95e65a86b9b47b5b131158d4a
Alexey <35376104+alex-arugon@users.noreply.github.com>
2019-03-20 12:04:35 +0200

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 cd 10.5.0.1
-docker-compose down --volumes
+docker-compose down
 cd ../
 cd 10.6.0.1
-docker-compose down --volumes
+docker-compose down
 cd ../
 cd 10.7.0.1
-docker-compose down --volumes
+docker-compose down
 cd ../
