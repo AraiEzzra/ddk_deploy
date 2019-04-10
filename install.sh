813306a5c118a64a2b0e01d53ff4295627f76cdb
Dmytro Mehed <d.mehed@sk-consulting.com.ua>
2019-04-10 16:29:34 +0300

    master: change split-api-core to master

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir data
 cd data
-git clone -b split-api-core https://github.com/AraiEzzra/DDKCORE.git
+git clone -b master https://github.com/AraiEzzra/DDKCORE.git
 cd ..
 sh volume-clean.sh
