d9d9939bac8f9a3dab0116729b271483f66a4d8c
Dmytro Mehed <d.mehed@sk-consulting.com.ua>
2019-04-15 09:28:19 +0300

    remove unsused[

--- test-start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-
-cd ./test
-docker-compose up --build -d
-cd ../
-docker exec -it ddknode-tests /bin/bash
--- test-stop.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-
-cd ./test
-docker-compose down --volumes
