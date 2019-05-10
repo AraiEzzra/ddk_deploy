53a355869ed46562ff2e8e34999d91c8a667eac9
Dmytro Mehed <d.mehed@sk-consulting.com.ua>
2019-05-10 20:15:30 +0300

    master: add history

--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+cd test
+docker-compose down --volumes
+cd ../
 cd 10.6.0.1
 docker-compose build ddk
 cd ../test
-docker-compose up --build -d ddk api db
-docker-compose up test
 rm -rf log.log
+docker-compose up --build -d ddk api db
 docker-compose logs ddk api >&1 >> log.log
+docker-compose up test
 docker-compose down --volumes
