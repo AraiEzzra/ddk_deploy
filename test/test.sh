a1e2aee40749989982b18f0d5e0f4f9fe204eade
Oleksii Fisenko <a.fisenko@sk-consulting.com.ua>
2019-05-15 17:32:08 +0300

    add environments for functional tests

--- test/test.sh
+++ test/test.sh
@@ -2,10 +2,9 @@
 
 cd test
 docker-compose down --volumes
-cd ../
-cd 10.6.0.1
+cd ../10.6.0.1
 docker-compose build ddk
-cd ../test
+cd ../test/integration
 rm -rf log.log
 docker-compose up --build -d ddk api db
 docker-compose logs ddk api >&1 >> log.log
