b20a75bbe1cfa21ed240522ae67bee08fc88db97
Oleksii Fisenko <a.fisenko@sk-consulting.com.ua>
2019-05-27 19:07:19 +0300

    add prepare environment for laboratory

---
+++ test/lab/test-lab.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+docker network create --subnet=10.0.0.0/9 --gateway=10.0.0.1 docker_ddk-testnet
+cd ../../
+cd 10.6.0.1
+docker-compose build ddk
+cd ../test/lab
+
+cd 10.10.0.1
+docker-compose up -d --build
+cd ../
+cd 10.11.0.1
+docker-compose up -d --build
+cd ../
+cd test-runner
+docker-compose up --build
