f716f21c53f44fa85f9c9eca31b5a754a4565d10
Aleksandr Shevchenko <axelsheva94@gmail.com>
2019-05-28 16:59:19 +0300

    Add 57 nodes

--- volume-clean.sh
+++ volume-clean.sh
@@ -2,6 +2,122 @@ cd 10.6.0.1
 docker-compose down --volumes
 cd ../10.7.0.1
 docker-compose down --volumes
+
+cd ../10.8.0.1
+docker-compose down --volumes
+cd ../10.9.0.1
+docker-compose down --volumes
+cd ../10.10.0.1
+docker-compose down --volumes
+cd ../10.11.0.1
+docker-compose down --volumes
+cd ../10.12.0.1
+docker-compose down --volumes
+cd ../10.13.0.1
+docker-compose down --volumes
+cd ../10.14.0.1
+docker-compose down --volumes
+cd ../10.15.0.1
+docker-compose down --volumes
+cd ../10.16.0.1
+docker-compose down --volumes
+cd ../10.17.0.1
+docker-compose down --volumes
+cd ../10.18.0.1
+docker-compose down --volumes
+cd ../10.19.0.1
+docker-compose down --volumes
+cd ../10.20.0.1
+docker-compose down --volumes
+cd ../10.21.0.1
+docker-compose down --volumes
+cd ../10.22.0.1
+docker-compose down --volumes
+cd ../10.23.0.1
+docker-compose down --volumes
+cd ../10.24.0.1
+docker-compose down --volumes
+cd ../10.25.0.1
+docker-compose down --volumes
+cd ../10.26.0.1
+docker-compose down --volumes
+cd ../10.27.0.1
+docker-compose down --volumes
+cd ../10.28.0.1
+docker-compose down --volumes
+cd ../10.29.0.1
+docker-compose down --volumes
+cd ../10.30.0.1
+docker-compose down --volumes
+cd ../10.31.0.1
+docker-compose down --volumes
+cd ../10.32.0.1
+docker-compose down --volumes
+cd ../10.33.0.1
+docker-compose down --volumes
+cd ../10.34.0.1
+docker-compose down --volumes
+cd ../10.35.0.1
+docker-compose down --volumes
+cd ../10.36.0.1
+docker-compose down --volumes
+cd ../10.37.0.1
+docker-compose down --volumes
+cd ../10.38.0.1
+docker-compose down --volumes
+cd ../10.39.0.1
+docker-compose down --volumes
+cd ../10.40.0.1
+docker-compose down --volumes
+cd ../10.41.0.1
+docker-compose down --volumes
+cd ../10.42.0.1
+docker-compose down --volumes
+cd ../10.43.0.1
+docker-compose down --volumes
+cd ../10.44.0.1
+docker-compose down --volumes
+cd ../10.45.0.1
+docker-compose down --volumes
+cd ../10.46.0.1
+docker-compose down --volumes
+cd ../10.47.0.1
+docker-compose down --volumes
+cd ../10.48.0.1
+docker-compose down --volumes
+cd ../10.49.0.1
+docker-compose down --volumes
+cd ../10.50.0.1
+docker-compose down --volumes
+cd ../10.51.0.1
+docker-compose down --volumes
+cd ../10.52.0.1
+docker-compose down --volumes
+cd ../10.53.0.1
+docker-compose down --volumes
+cd ../10.54.0.1
+docker-compose down --volumes
+cd ../10.55.0.1
+docker-compose down --volumes
+cd ../10.56.0.1
+docker-compose down --volumes
+cd ../10.57.0.1
+docker-compose down --volumes
+cd ../10.58.0.1
+docker-compose down --volumes
+cd ../10.59.0.1
+docker-compose down --volumes
+cd ../10.60.0.1
+docker-compose down --volumes
+cd ../10.61.0.1
+docker-compose down --volumes
+cd ../10.62.0.1
+docker-compose down --volumes
+cd ../10.63.0.1
+docker-compose down --volumes
+cd ../10.64.0.1
+docker-compose down --volumes
+
 cd ../10.5.0.1
 docker-compose down --volumes
 cd ../
