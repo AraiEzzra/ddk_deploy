9ca51cae46ea6c2128b19a9c377209df63b53614
Dmytro Mekhed <d.mehed@eska.global>
2019-10-24 13:01:44 +0300

    fix_version: fix_version

--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@
 mkdir data
 cd data
 git clone -b master https://github.com/AraiEzzra/DDKCORE.git
+git clone -b master https://github.com/AraiEzzra/DDK.REGISTRY.git
 cd ..
 sh volume-clean.sh
+cd 10.6.0.1
+echo "Stop docker container after success build";
+docker-compose up watch
